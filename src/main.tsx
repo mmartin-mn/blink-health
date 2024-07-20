98db901b723b75c26d159c96a65a17d82b58a6ab
mmartin-mn <mjmartin3394@gmail.com>
2024-07-19 20:00:49 -0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,42 @@
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate
+} from "react-router-dom";
+import App from './App.tsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      {
+        index: true,
+        element: <Navigate replace to="/drugs/search" />
+      },
+      {
+        path: '/drugs',
+        children: [
+          {
+            index: true,
+            element: <Navigate replace to="/drugs/search" />
+          },
+          {
+            path: "search",
+            element: <div>Search</div>,
+            index: true
+          },
+          {
+            path: ":drugName",
+            element: <div>Drug Info</div>,
+          },
+        ],
+      }
+    ]
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+)
